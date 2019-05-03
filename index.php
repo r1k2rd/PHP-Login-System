0470e061bbdc3671270c50c906671767cc55be9a
r1k2rd <43808941+r1k2rd@users.noreply.github.com>
2019-05-03 10:00:35 +0300

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?php echo "Hello world"; ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- UIkit CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/css/uikit.min.css" />
+
+		<!-- UIkit JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.1.4/js/uikit-icons.min.js"></script>
+
+	</head>
+	<body>
+
+		<div class="uk-section uk-container uk-text-center">
+			<form class="uk-form-horizontal uk-margin-large js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-horizontal-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-horizontal-text" type="email" placeholder="email@email.com" required="required">
+			        </div>
+			    </div>
+
+			     <div class="uk-margin">
+			        <label class="uk-form-label" for="form-horizontal-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-horizontal-text" type="password" placeholder="Your password" required="required">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			    	<button class="uk-button uk-button-default" type="submit">LOGIN</button>
+			    </div>
+
+			</form>
+		</div>
+		
+	</body>
+</html>
